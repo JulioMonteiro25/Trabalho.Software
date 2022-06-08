74ec0211fda4b9985e41dd547cf54d7408dd4c73
Júlio Monteiro <102265419+JulioMonteiro25@users.noreply.github.com>
2022-06-07 21:59:15 -0300

    Add files via upload

---
+++ CookieSys ATT/CookieSys/src/cookiesys/CookieSys.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cookiesys;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author 202118753
+ */
+public class CookieSys {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+         new Login().setVisible(true);
+         
+
+
+    }}
+          
+           
+        
+
+
+              
+          
+    
+    
+
